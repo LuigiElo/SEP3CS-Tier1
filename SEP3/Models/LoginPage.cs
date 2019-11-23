277d04709adfd50b85f5562ad06349cc61c10002
filipkrupa <fkrupa98@gmail.com>
2019-11-23 02:33:29 +0100

    LogIn-1

---
+++ SEP3/Models/LoginPage.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEP3.Models
+{
+    public class LoginPage : PageModel
+    {
+        //get data from database:
+        private UserContext context;
+
+        public LoginPage(UserContext context)
+        {
+            this.context = context;
+        }
+
+        //defining the Class, which contains the GUI input:
+        public class InputModel
+        {
+            [Required] public string Name { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+        }
+        //binding the variable to View
+
+        [BindProperty] public InputModel Input { get; set; }
+
+        public async Task OnGetAsync()
+        {
+        }
+        
+        public async Task<ActivationResult> OnPostLoginAsync()
+        
+        
+        
+
+
+
+
+
+
+
+    }
+
+    
+}
