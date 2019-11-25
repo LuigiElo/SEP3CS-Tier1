b32bd7f570a4074900ecc8d009d35080ac1a9938
LuigiElo <LuigiElo@users.noreply.github.com>
2019-11-25 10:30:53 +0100

    New Index

--- SEP3/Pages/Index.cshtml.cs
+++ SEP3/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SEP3.Models;
 
 namespace SEP3.Pages
 {
@@ -9,10 +12,13 @@ namespace SEP3.Pages
     {
         [BindProperty, Required(ErrorMessage = "Please supply a Name" )]  public string Name { get; set; }
         [BindProperty, Required(ErrorMessage = "Please supply a Password" )]  public string Password { get; set; }
-        public string Username { get; set; }
-        public object Login { get; set; }
-        public object ConfPassword { get; set; }
-        public object Email { get; set; }
+        [BindProperty, Required(ErrorMessage = "Please supply a Username" )] public string Username { get; set; }
+        
+        [BindProperty, Required(ErrorMessage = "Please supply a Password" )] public string ConfPassword { get; set; }
+        [BindProperty, Required(ErrorMessage = "Please supply a Email" )]public string Email { get; set; }
+        
+        public Person Login { get; set; }
+        public Person Register { get; set; }
 
         private readonly ILogger<IndexModel> _logger;
 
@@ -23,6 +29,14 @@ namespace SEP3.Pages
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            
+            await Task.Delay(0);
+            Console.WriteLine(Username);
+            return RedirectToPage("CreatedParty");
         }
     }
 }
