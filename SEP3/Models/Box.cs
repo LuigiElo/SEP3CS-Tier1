fa9c860034fbbdd96afa5708663ccd1899064eed
Michal <mincoslav@gmail.com>
2019-12-06 17:36:04 +0100

    Box

---
+++ SEP3/Models/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SEP3.Models
+{
+    
+    [Serializable]
+    public class Box
+    {
+        
+        public Party Party { get; set; }
+        
+        public List<Item> Items { get; set; }
+        
+        public List<Person> People { get; set; }
+        
+    }
+}
